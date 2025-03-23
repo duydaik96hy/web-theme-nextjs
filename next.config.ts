eb3fc58fed80a04a42a10bab539228e6301e190f
trinhtx <trinh.tx@teko.vn>
2025-03-23 20:15:15 +0700

    Adđ config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,29 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack(config) {
+    config.module.rules.forEach((rule: { test: { toString: () => string | string[]; }; use: any[]; }) => {
+      if (rule.test && rule.test.toString().includes('css')) {
+        rule.use.forEach((loader) => {
+          if (loader.loader === 'css-loader') {
+            loader.options = {
+              ...loader.options,
+              // Disable CSS Modules completely
+              modules: false,
+              // Enable class name renaming with a hash (adjust to your preference)
+              localIdentName: '[hash:base64:8]', // Renaming class with base64 hash
+            };
+          }
+        });
+      }
+    });
+    return config;
   },
 };
 
